a60ffe05219c278e106e57b0cd1efba5750f3958
Pratik Gorade <pratikgorade0@gmail.com>
2019-11-13 18:44:47 +0530

    RSA Done for numeric values (but it needs bigger prime number rn)

--- IS/rsa_implementation.py
+++ IS/rsa_implementation.py
@@ -5,6 +5,8 @@ Panel : 2
 Batch : B2
 Roll No. : PB02
 """
+from math import sqrt
+from itertools import count, islice
 
 def gcd(a, b):
     if b == 0:
@@ -12,18 +14,37 @@ def gcd(a, b):
     else:
         return gcd(b, a%b)
 
-def main():
-    p = 23
-    q = 37
-    n = p*q
-    z = (p-1)*(q-1)
-    print('z : ',z)
-    for e in range(2, z-1):
-        if gcd(e, z)==1:
-            break
-    print('e : ', e)
-    k = 2
+def isPrime(n):
+    if n < 2:
+        return False
 
+    for number in islice(count(2), int(sqrt(n) - 1)):
+        if n % number == 0:
+            return False
+
+    return True
+
+def main():
+    p = int(input("Enter any prime number : "))
+    while isPrime(p) == False:
+        p = int(input("{} is not prime number, Enter again : ".format(p)))
+    q = int(input("Enter one more prime number : "))
+    while isPrime(q) == False:
+        q = int(input("{} is not prime number, Enter again : ".format(q)))
+    n = p*q
+    phi = (p-1)*(q-1)
+    for e in range(2, phi):
+        if gcd(e, phi)==1:
+            break
+    for d in range(2, phi):
+        if (d*e)%phi==1:
+            break
+
+    m = int(input("Enter any numerical message to encrypt : "))
+    encrypted_message = pow(m, e)%n
+    print("Encrypted message is :", encrypted_message)
+    decrypted_message = pow(encrypted_message, d)%n
+    print("Decrypted message is : ", decrypted_message)
 #calculate d using (d*e) mod z = 1
 if __name__ == '__main__':
     main()
